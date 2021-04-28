98b1aa49d0d20cdfa143f5999fb0982e09b416a0
Celine_lhp5 <celine.louvel.76@outlook.fr>
2021-04-28 15:58:28 +0200

    footermodif

--- public/application/views/header.php
+++ public/application/views/header.php
@@ -16,11 +16,11 @@
 
     <header>
         <nav class="navbar navbar-expand-lg">
-           
+
                 <a class="navbar-brand" href="#">
                     <img src="/assets/img/Leboncoin.fr_Logo_2016.svg" width="100" height="38" class="d-inline-block align-top" alt="">
                 </a>
-         
+                
             <!-- menu burger -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
